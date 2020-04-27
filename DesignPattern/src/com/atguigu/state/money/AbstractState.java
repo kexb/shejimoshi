4ede88e538819f6ad3190b4b712f54ac1024c413
e <1061210163@qq.com>
2020-04-28 01:10:43 +0800

    状态注释

--- DesignPattern/src/com/atguigu/state/money/AbstractState.java
+++ DesignPattern/src/com/atguigu/state/money/AbstractState.java
@@ -2,46 +2,53 @@ package com.atguigu.state.money;
 
 public abstract class AbstractState implements State {
 
-	protected static final RuntimeException EXCEPTION = new RuntimeException("操作流程不允许");
+    protected static final RuntimeException EXCEPTION = new RuntimeException("操作流程不允许");
 
-	//抽象类，默认实现了 State 接口的所有方法
-	//该类的所有方法，其子类(具体的状态类)，可以有选择的进行重写
-	
+    //抽象类，默认实现了 State 接口的所有方法
+    //该类的所有方法，其子类(具体的状态类)，可以有选择的进行重写
+    //电审
     @Override
     public void checkEvent(Context context) {
         throw EXCEPTION;
     }
 
+    //电审失败
     @Override
     public void checkFailEvent(Context context) {
         throw EXCEPTION;
     }
 
+    //定价发布
     @Override
     public void makePriceEvent(Context context) {
         throw EXCEPTION;
     }
 
+    //接单
     @Override
     public void acceptOrderEvent(Context context) {
         throw EXCEPTION;
     }
 
+    //无人接单失效
     @Override
     public void notPeopleAcceptEvent(Context context) {
         throw EXCEPTION;
     }
 
+    //付款
     @Override
     public void payOrderEvent(Context context) {
         throw EXCEPTION;
     }
 
+    //接单有人支付失效
     @Override
     public void orderFailureEvent(Context context) {
         throw EXCEPTION;
     }
 
+    //反馈
     @Override
     public void feedBackEvent(Context context) {
         throw EXCEPTION;
--- DesignPattern/src/com/atguigu/state/money/Context.java
+++ DesignPattern/src/com/atguigu/state/money/Context.java
@@ -5,48 +5,56 @@ public class Context extends AbstractState{
 	//当前的状态 state, 根据我们的业务流程处理，不停的变化
 	private State state;
 
+	//电审
     @Override
     public void checkEvent(Context context) {
         state.checkEvent(this);
         getCurrentState();
     }
 
+    //电审失败
     @Override
     public void checkFailEvent(Context context) {
         state.checkFailEvent(this);
         getCurrentState();
     }
 
+    //定价发布
     @Override
     public void makePriceEvent(Context context) {
         state.makePriceEvent(this);
         getCurrentState();
     }
 
+    //接单
     @Override
     public void acceptOrderEvent(Context context) {
         state.acceptOrderEvent(this);
         getCurrentState();
     }
 
+    //无人接单失效
     @Override
     public void notPeopleAcceptEvent(Context context) {
         state.notPeopleAcceptEvent(this);
         getCurrentState();
     }
 
+    //付款
     @Override
     public void payOrderEvent(Context context) {
         state.payOrderEvent(this);
         getCurrentState();
     }
 
+    //接单有人支付失效
     @Override
     public void orderFailureEvent(Context context) {
         state.orderFailureEvent(this);
         getCurrentState();
     }
 
+    //反馈
     @Override
     public void feedBackEvent(Context context) {
         state.feedBackEvent(this);
